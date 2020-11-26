b733b7764e3db94ded998a7e70df5249ac48d1c6
Foxdaddi <rensvdriel@hotmail.com>
2020-11-26 19:53:18 +0100

    mediaquery fix

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -29,54 +29,56 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height * 1,
-        width: MediaQuery.of(context).size.width * 1,
-        child: PageView(
-          controller: _pageController,
-          children: _screens,
-          onPageChanged: _onPageChanged,
-          physics: PageScrollPhysics(),
+    return Container(
+      height: MediaQuery.of(context).size.height * 1,
+      width: MediaQuery.of(context).size.width * 1,
+      child: Scaffold(
+        body: Container(
+          child: PageView(
+            controller: _pageController,
+            children: _screens,
+            onPageChanged: _onPageChanged,
+            physics: PageScrollPhysics(),
+          ),
         ),
-      ),
-      bottomNavigationBar: Container(
-        height: MediaQuery.of(context).size.height * 0.07,
-        child: BottomNavigationBar(
-          backgroundColor: Color(0xFFF6F2F1),
-          elevation: 0,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          onTap: _onItemTapped,
-          items: [
-            BottomNavigationBarItem(
-                icon: Container(
-                  height: MediaQuery.of(context).size.height * 0.03,
-                  child: Image.asset('images/diagnoseicon.png',
-                      color: _selectedIndex == 0
-                          ? Color(0xFF56A67B)
-                          : Colors.grey),
-                ),
-                title: Text('diagnosis')),
-            BottomNavigationBarItem(
-                icon: Container(
-                  height: MediaQuery.of(context).size.height * 0.03,
-                  child: Image.asset('images/listicon.png',
-                      color: _selectedIndex == 1
-                          ? Color(0xFF56A67B)
-                          : Colors.grey),
-                ),
-                title: Text('complete list')),
-            BottomNavigationBarItem(
-                icon: Container(
-                  height: MediaQuery.of(context).size.height * 0.03,
-                  child: Image.asset('images/searchicon.png',
-                      color: _selectedIndex == 2
-                          ? Color(0xFF56A67B)
-                          : Colors.grey),
-                ),
-                title: Text('search')),
-          ],
+        bottomNavigationBar: Container(
+          height: MediaQuery.of(context).size.height * 0.07,
+          child: BottomNavigationBar(
+            backgroundColor: Color(0xFFF6F2F1),
+            elevation: 0,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            onTap: _onItemTapped,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Container(
+                    height: MediaQuery.of(context).size.height * 0.03,
+                    child: Image.asset('images/diagnoseicon.png',
+                        color: _selectedIndex == 0
+                            ? Color(0xFF56A67B)
+                            : Colors.grey),
+                  ),
+                  title: Text('diagnosis')),
+              BottomNavigationBarItem(
+                  icon: Container(
+                    height: MediaQuery.of(context).size.height * 0.03,
+                    child: Image.asset('images/listicon.png',
+                        color: _selectedIndex == 1
+                            ? Color(0xFF56A67B)
+                            : Colors.grey),
+                  ),
+                  title: Text('complete list')),
+              BottomNavigationBarItem(
+                  icon: Container(
+                    height: MediaQuery.of(context).size.height * 0.03,
+                    child: Image.asset('images/searchicon.png',
+                        color: _selectedIndex == 2
+                            ? Color(0xFF56A67B)
+                            : Colors.grey),
+                  ),
+                  title: Text('search')),
+            ],
+          ),
         ),
       ),
     );
